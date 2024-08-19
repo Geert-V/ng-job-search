bbbf921bd014956c46d5ae8721c31ab682ad532c
Geert Verstegen <geert.verstegen@mstack.nl>
2024-08-19 11:30:46 +0200

    Implemented favorites page

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Job } from '../core/models/job.model';
+import { JobService } from '../core/services/job.service';
+import { AsyncPipe } from '@angular/common';
+import { JobItemComponent } from '../components/job-item/job-item.component';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    JobItemComponent
+  ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+  public jobs$!: Observable<Job[]>;
 
+  constructor(private jobService: JobService) {
+  }
+
+  public ngOnInit(): void {
+    this.jobs$ = this.jobService.getFavorites();
+  }
 }
